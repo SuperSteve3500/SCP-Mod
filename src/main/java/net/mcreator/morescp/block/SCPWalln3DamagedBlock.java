bf03b7ef18759779f634a21892f9f2da30ac17e5
SuperSteve3500 <joaom@DESKTOP-F7MMB9J.lan>
2020-06-16 11:38:53 +0100

    hi

---
+++ src/main/java/net/mcreator/morescp/block/SCPWalln3DamagedBlock.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.morescp.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.morescp.itemgroup.SCPBlocksItemGroup;
+import net.mcreator.morescp.MorescpModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@MorescpModElements.ModElement.Tag
+public class SCPWalln3DamagedBlock extends MorescpModElements.ModElement {
+	@ObjectHolder("morescp:scp_walln_3_damaged")
+	public static final Block block = null;
+	public SCPWalln3DamagedBlock(MorescpModElements instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(SCPBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("scp_walln_3_damaged");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
